876ff700f31f1024c258e315c4b49375b6b95269
Dabo Ross <dabodialga@gmail.com>
2013-07-07 23:42:36 -0700

    Initial Commit

---
+++ src/main/java/net/daboross/mavenbukkitcreator/Main.java
@@ -0,0 +1,72 @@
+/*
+ * Author: Dabo Ross
+ * Website: www.daboross.net
+ * Email: daboross@daboross.net
+ */
+package net.daboross.mavenbukkitcreator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import static java.lang.System.out;
+
+/**
+ *
+ * @author daboross
+ */
+public class Main {
+
+    private static BufferedReader in;
+
+    private static void init() {
+        out.println("Welcome to Maven Bukkit Project Creator");
+        Reader r = new InputStreamReader(System.in);
+        in = new BufferedReader(r);
+    }
+
+    private static String ask(String question) throws IOException {
+        out.printf(question + "\n> ");
+        return in.readLine();
+    }
+
+    private static boolean askBoolean(String question) throws IOException {
+        out.printf(question + "\n> ");
+        while (true) {
+            String line = in.readLine();
+            if (line.equalsIgnoreCase("yes")) {
+                return true;
+            } else if (line.equalsIgnoreCase("no")) {
+                return false;
+            } else {
+                out.printf("'" + line + "' isn't a valid answer.\n(yes or no)> ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            init();
+            makeProject();
+        } catch (IOException ioe) {
+            ioe.printStackTrace(System.err);
+        }
+    }
+
+    private static void makeProject() throws IOException {
+        String name, desc;
+        while (true) {
+            name = ask("What should the project be called?");
+            if (askBoolean("Project will be called '" + name + "'. Is this OK?")) {
+                break;
+            }
+        }
+        while (true) {
+            desc = ask("What should the project's description be?");
+            if (askBoolean("Project will be described as '" + desc + "'. Is this OK?")) {
+                break;
+            }
+        }
+        new ProjectCreator(name, desc).create();
+    }
+}
---
+++ src/main/java/net/daboross/mavenbukkitcreator/ProjectCreator.java
@@ -0,0 +1,75 @@
+/*
+ * Author: Dabo Ross
+ * Website: www.daboross.net
+ * Email: daboross@daboross.net
+ */
+package net.daboross.mavenbukkitcreator;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+
+/**
+ *
+ * @author daboross
+ */
+public class ProjectCreator {
+
+    private final String name, desc;
+    private File projectDir, javaDir, resourceDir, targetMainFile, targetPluginYaml, targetPomXml, targetGitIgnore;
+
+    public ProjectCreator(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public void create() throws IOException {
+        getDirs();
+        copyFile(getClass().getResourceAsStream("/template/Main.java.new"), targetMainFile);
+        copyFile(getClass().getResourceAsStream("/template/plugin.yml.new"), targetPluginYaml);
+        copyFile(getClass().getResourceAsStream("/template/pom.xml.new"), targetPomXml);
+        copyFile(getClass().getResourceAsStream("/template/GitIgnore.new"), targetGitIgnore);
+    }
+
+    private void getDirs() {
+        projectDir = new File(name);
+        if (projectDir.exists()) {
+            System.err.println("Project Directory Exists!");
+            System.exit(1);
+        }
+        projectDir.mkdirs();
+        javaDir = new File(new File(new File(new File(new File(new File(new File(projectDir, "src"), "main"), "java"), "net"), "daboross"), "bukkitdev"), name.toLowerCase());
+        resourceDir = new File(new File(new File(projectDir, "src"), "main"), "resources");
+        javaDir.mkdirs();
+        resourceDir.mkdirs();
+        targetMainFile = new File(javaDir, name + ".java");
+        targetPluginYaml = new File(resourceDir, "plugin.yml");
+        targetPomXml = new File(projectDir, "pom.xml");
+        targetGitIgnore = new File(projectDir, ".gitignore");
+    }
+
+    private void copyFile(InputStream input, File output) throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(input);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        FileWriter fileWriter = new FileWriter(output);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        PrintWriter printWriter = new PrintWriter(bufferedWriter);
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            printWriter.println(line.replaceAll(Matcher.quoteReplacement("$$NAME"), name)
+                    .replaceAll(Matcher.quoteReplacement("$$LOWER"), name.toLowerCase())
+                    .replaceAll(Matcher.quoteReplacement("$$DESC"), desc));
+        }
+        bufferedReader.close();
+        inputStreamReader.close();
+        printWriter.close();
+        bufferedWriter.close();
+        fileWriter.close();
+    }
+}
