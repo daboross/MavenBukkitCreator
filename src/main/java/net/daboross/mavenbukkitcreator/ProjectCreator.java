12bb48f8991ac223371bf38bfea866a6d732d78f
Dabo Ross <dabodialga@gmail.com>
2013-07-28 15:25:37 -0700

    Formatting

--- src/main/java/net/daboross/mavenbukkitcreator/ProjectCreator.java
+++ src/main/java/net/daboross/mavenbukkitcreator/ProjectCreator.java
@@ -32,65 +32,65 @@ import java.util.regex.Matcher;
  */
 public class ProjectCreator {
 
-	private final String name, desc;
-	private File projectDirectory, sourceDirectory, resourceDirectory,
-			targetPluginFile, targetPluginYaml, targetPomXml, targetGitIgnore, targetTravisYaml, targetLicense;
-	private final String gitProjectName;
-	private final boolean isPluginRequest;
+    private final String name, desc;
+    private File projectDirectory, sourceDirectory, resourceDirectory,
+            targetPluginFile, targetPluginYaml, targetPomXml, targetGitIgnore, targetTravisYaml, targetLicense;
+    private final String gitProjectName;
+    private final boolean isPluginRequest;
 
-	public ProjectCreator(String name, String desc, String gitProjectName, boolean isPluginRequest) {
-		this.name = name;
-		this.desc = desc;
-		this.gitProjectName = gitProjectName;
-		this.isPluginRequest = isPluginRequest;
-	}
+    public ProjectCreator(String name, String desc, String gitProjectName, boolean isPluginRequest) {
+        this.name = name;
+        this.desc = desc;
+        this.gitProjectName = gitProjectName;
+        this.isPluginRequest = isPluginRequest;
+    }
 
-	public void create() throws IOException, InterruptedException {
-		getDirs();
-		copyFile(getClass().getResourceAsStream("/template/Plugin.java.new"), targetPluginFile);
-		copyFile(getClass().getResourceAsStream("/template/plugin.yml.new"), targetPluginYaml);
-		copyFile(getClass().getResourceAsStream("/template/pom.xml.new"), targetPomXml);
-		copyFile(getClass().getResourceAsStream("/template/.gitignore.new"), targetGitIgnore);
-		copyFile(getClass().getResourceAsStream("/template/.travis.yml.new"), targetTravisYaml);
-		copyFile(getClass().getResourceAsStream("/LICENSE"), targetLicense);
-		new GitCreator(projectDirectory, gitProjectName, desc).run(false);
-	}
+    public void create() throws IOException, InterruptedException {
+        getDirs();
+        copyFile(getClass().getResourceAsStream("/template/Plugin.java.new"), targetPluginFile);
+        copyFile(getClass().getResourceAsStream("/template/plugin.yml.new"), targetPluginYaml);
+        copyFile(getClass().getResourceAsStream("/template/pom.xml.new"), targetPomXml);
+        copyFile(getClass().getResourceAsStream("/template/.gitignore.new"), targetGitIgnore);
+        copyFile(getClass().getResourceAsStream("/template/.travis.yml.new"), targetTravisYaml);
+        copyFile(getClass().getResourceAsStream("/LICENSE"), targetLicense);
+        new GitCreator(projectDirectory, gitProjectName, desc).run(false);
+    }
 
-	private void getDirs() {
-		projectDirectory = new File((isPluginRequest ? "Request-" : "Private-") + name);
-		if (projectDirectory.exists()) {
-			System.err.println("Project Directory Exists!");
-			System.exit(1);
-		}
-		projectDirectory.mkdirs();
-		sourceDirectory = new File(new File(new File(new File(new File(new File(new File(projectDirectory, "src"), "main"), "java"), "net"), "daboross"), "bukkitdev"), name.toLowerCase());
-		resourceDirectory = new File(new File(new File(projectDirectory, "src"), "main"), "resources");
-		sourceDirectory.mkdirs();
-		resourceDirectory.mkdirs();
-		targetPluginFile = new File(sourceDirectory, name + "Plugin.java");
-		targetPluginYaml = new File(resourceDirectory, "plugin.yml");
-		targetLicense = new File(projectDirectory, "LICENSE");
-		targetPomXml = new File(projectDirectory, "pom.xml");
-		targetTravisYaml = new File(projectDirectory, ".travis.yml");
-		targetGitIgnore = new File(projectDirectory, ".gitignore");
-	}
+    private void getDirs() {
+        projectDirectory = new File((isPluginRequest ? "Request-" : "Private-") + name);
+        if (projectDirectory.exists()) {
+            System.err.println("Project Directory Exists!");
+            System.exit(1);
+        }
+        projectDirectory.mkdirs();
+        sourceDirectory = new File(new File(new File(new File(new File(new File(new File(projectDirectory, "src"), "main"), "java"), "net"), "daboross"), "bukkitdev"), name.toLowerCase());
+        resourceDirectory = new File(new File(new File(projectDirectory, "src"), "main"), "resources");
+        sourceDirectory.mkdirs();
+        resourceDirectory.mkdirs();
+        targetPluginFile = new File(sourceDirectory, name + "Plugin.java");
+        targetPluginYaml = new File(resourceDirectory, "plugin.yml");
+        targetLicense = new File(projectDirectory, "LICENSE");
+        targetPomXml = new File(projectDirectory, "pom.xml");
+        targetTravisYaml = new File(projectDirectory, ".travis.yml");
+        targetGitIgnore = new File(projectDirectory, ".gitignore");
+    }
 
-	private void copyFile(InputStream input, File output) throws IOException {
-		InputStreamReader inputStreamReader = new InputStreamReader(input);
-		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-		FileWriter fileWriter = new FileWriter(output);
-		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-		PrintWriter printWriter = new PrintWriter(bufferedWriter);
-		String line;
-		while ((line = bufferedReader.readLine()) != null) {
-			printWriter.println(line.replaceAll(Matcher.quoteReplacement("$$NAME"), name)
-					.replaceAll(Matcher.quoteReplacement("$$LOWER"), name.toLowerCase())
-					.replaceAll(Matcher.quoteReplacement("$$DESC"), desc));
-		}
-		bufferedReader.close();
-		inputStreamReader.close();
-		printWriter.close();
-		bufferedWriter.close();
-		fileWriter.close();
-	}
+    private void copyFile(InputStream input, File output) throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(input);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        FileWriter fileWriter = new FileWriter(output);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        PrintWriter printWriter = new PrintWriter(bufferedWriter);
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            printWriter.println(line.replaceAll(Matcher.quoteReplacement("$$NAME"), name)
+                    .replaceAll(Matcher.quoteReplacement("$$LOWER"), name.toLowerCase())
+                    .replaceAll(Matcher.quoteReplacement("$$DESC"), desc));
+        }
+        bufferedReader.close();
+        inputStreamReader.close();
+        printWriter.close();
+        bufferedWriter.close();
+        fileWriter.close();
+    }
 }
