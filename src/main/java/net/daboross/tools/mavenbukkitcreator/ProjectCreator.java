3e590862c9d9d681708944d6f3053640498f3f8c
Dabo Ross <daboross@daboross.net>
2013-07-29 12:52:38 -0700

    Changed package

--- src/main/java/net/daboross/tools/mavenbukkitcreator/ProjectCreator.java
+++ src/main/java/net/daboross/tools/mavenbukkitcreator/ProjectCreator.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package net.daboross.mavenbukkitcreator;
+package net.daboross.tools.mavenbukkitcreator;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
